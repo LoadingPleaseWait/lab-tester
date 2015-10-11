e63bea7e91542e00303ecb1fb37fa63c906d5592
LoadingPleaseWait <michael@murphey.org>
2015-10-10 21:42:52 -0500

    Added user and help options and triangle lab

--- tester.sh
+++ tester.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-python3 /home/$1/$3 < $2/input.txt # actually run the program
+# first parameter should be username second should be key-directory third is assignment-file
+function test {
+  python3 /home/$1/$3 < $2/input.txt # actually run the program
 
-# compare output to sample correct output
-output=$(python3 /home/$1/$3 < $2/input.txt)
-answer=$(cat $2/output.txt)
-if [[ "$output" == "$answer" ]]; then
-  echo "Output matches"
+  # compare output to sample output
+  output=$(python3 /home/$1/$3 < $2/input.txt)
+  answer=$(cat $2/output.txt)
+  if [[ "$output" == "$answer" ]]; then
+    echo "Output matches"
+  else
+    echo "Output does not match"
+  fi
+}
+
+if [[ $1 = "--help" || $1 = "-h" ]]; then
+  echo "Usage: ./tester.sh [option] key-directory assignment-file"
+elif [[ $1 = "--user"  || $1 = "-u" ]]; then
+  test $2 $3 $4
 else
-  echo "Output does not match"
+  # loop through home directory and test assignment-file for all users
+  for user in "/home/*"
+  do
+    username=$(echo $user)
+    username=${username/"/home/"/}
+    echo $username
+    test $username $2 $3
+  done
 fi
